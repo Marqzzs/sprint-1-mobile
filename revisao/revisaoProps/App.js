bbac71d34cc53b2d30aadc7a7144a7d9b376613f
Maurício Marques Pereira <55358961879@info.local>
2024-01-29 09:49:30 -0300

    finalizao dos exercicios inicias

--- revisao/revisaoProps/App.js
+++ revisao/revisaoProps/App.js
@@ -1,15 +1,37 @@
-import { SafeAreaView, StatusBar} from 'react-native';
+import { FlatList, SafeAreaView, StatusBar} from 'react-native';
 import Person from './scr/components/Person/Person';
+import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
 
 export default function App() {
+
+  let [fontsLoaded, fontError] = useFonts({
+    Poppins_400Regular
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  const peopleList = [
+    {id: '1', name: 'Mauricio', age: '18'},
+    {id: '2', name: 'Isabella', age: '19'},
+    {id: '3', name: 'Clara', age: '20'},
+    {id: '4', name: 'Julia', age: '16'},
+    {id: '5', name: 'Vitoria', age: '16'},
+  ]
   return (
     <SafeAreaView>
       <StatusBar />
+    {/* usando flatList */}
+    <FlatList 
+    data={peopleList}
+    keyExtractor={(item) => item.id}
 
-      <Person name={'Mauricio'} age={'18'}/>
-      <Person name={'Isabella'} age={'19'}/>
-      <Person name={'Clara'} age={'20'}/>
-      <Person name={'Julia'} age={'17'}/>
+    renderItem={({item}) => (
+      //exigir cada item da lista
+      <Person name={item.name} age={item.age} />
+    )}
+    />
 
     </SafeAreaView>
   );
--- revisao/revisaoProps/scr/components/Person/Person.js
+++ revisao/revisaoProps/scr/components/Person/Person.js
@@ -1,26 +1,35 @@
 //componente Person
-
-import {View, Text,StyleSheet } from "react-native"
+import { View, Text, StyleSheet } from "react-native";
+import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
 //props: name, age
-const Person = ({name, age}) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Nome: {name}</Text>
-            <Text style={styles.text}>Age: {age}</Text>
-        </View>
-    );
-}
+const Person = ({ name, age }) => {
+  let [fontsLoaded, fontError] = useFonts({
+    Poppins_400Regular,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Nome: {name}</Text>
+      <Text style={styles.text}>Age: {age}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#e0e0e0",
-        padding: 10,
-        margin: 10,
-        borderRadius: 8,
-    },
-    text: {
-        color: "green"
-    }
-})
+  container: {
+    backgroundColor: "#000000",
+    padding: 10,
+    margin: 10,
+    borderRadius: 8,
+  },
+  text: {
+    color: "green",
+    fontFamily: 'Poppins_400Regular'
+  },
+});
 
 export default Person;
